71a87680f9a58f5eb8c892d145a7ae2e9cee87c6
ihjacobs925 <ihjacobs925@gmail.com>
2021-02-15 13:32:12 -0600

    routes stubbed

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = (props) => {
+    return (
+        <div className="home-container">
+            <h1>About Me</h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = (props) => {
+    return (
+        <div className="home-container">
+            <h1>Blog</h1>
+        </div>
+    )
+}
+
+export default Blog;
